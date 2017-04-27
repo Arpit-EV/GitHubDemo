ee69390b8dcdd05508f7162ece4ee74c2dd81c5d
Arpit Sharma <Arpit@Arpits-MacBook-Pro.local>
2017-04-27 13:40:38 +0530

    added VC

---
+++ GitHubDemo/imViewController.h
@@ -0,0 +1,13 @@
+//
+//  imViewController.h
+//  GitHubDemo
+//
+//  Created by Arpit Sharma on 27/04/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface imViewController : UIViewController
+
+@end
---
+++ GitHubDemo/imViewController.m
@@ -0,0 +1,37 @@
+//
+//  imViewController.m
+//  GitHubDemo
+//
+//  Created by Arpit Sharma on 27/04/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import "imViewController.h"
+
+@interface imViewController ()
+
+@end
+
+@implementation imViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
