ec47d090d001c3990a88b8a7ba816ccbf413d4ea
Arpit Sharma <Arpit@Arpits-MacBook-Pro.local>
2017-04-27 12:36:26 +0530

    New Start

--- GitHubDemo/SecondViewController.m
+++ GitHubDemo/SecondViewController.m
@@ -1,38 +1,40 @@
 //
-//  ViewController.m
+//  SecondViewController.m
 //  GitHubDemo
 //
-//  Created by Arpit Sharma on 26/04/17.
+//  Created by Arpit Sharma on 27/04/17.
 //  Copyright © 2017 Edutainment Ventures. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "SecondViewController.h"
+#import "ThirdViewController.h"
 
-@interface ViewController ()
+@interface SecondViewController ()
 
 @end
 
-@implementation ViewController
+@implementation SecondViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [[[[UIApplication sharedApplication] windows] objectAtIndex:0] setBackgroundColor:[UIColor whiteColor]];
+    // Do any additional setup after loading the view.
+    
+    UIBarButtonItem *btnBack=[[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStylePlain target:self action:@selector(btnClickedBack:)];
+    self.navigationItem.leftBarButtonItem=btnBack;
     
     UILabel *lblTitle = [[UILabel alloc]init];
-    [lblTitle setText:@"Click the button bellow to go to next View Controller"];
+    [lblTitle setText:@"Arpit's Second View Controller"];
     [lblTitle setNumberOfLines:0];
     [lblTitle setTextAlignment:NSTextAlignmentCenter];
     [lblTitle setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addSubview: lblTitle];
-
+    
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:lblTitle attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeadingMargin multiplier:1.0 constant:0.0]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:lblTitle attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailingMargin multiplier:1.0 constant:0.0]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:lblTitle attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:lblTitle attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
-
-
+    
+    
     [lblTitle layoutIfNeeded];
     
     
@@ -57,16 +59,31 @@
 -(void)btnClickedGoToNextVC:(UIButton *)sender
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        SecondViewController *objSecondViewController = [[SecondViewController alloc]init];
-        [self.navigationController pushViewController:objSecondViewController animated:YES];
+        ThirdViewController *objThirdViewController = [[ThirdViewController alloc]init];
+        [self.navigationController pushViewController:objThirdViewController animated:YES];
     });
     
 }
+
+-(void)btnClickedBack:(UIBarButtonItem *)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
